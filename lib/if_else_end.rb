71fb6992c52142124622d000cb3d1386ad7800a8
Arnie Serrano <arniekidd@gmail.com>
2020-06-19 19:43:32 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,2 @@
 # Write your solution here
-chance_of_rain=0.2
-puts "Pack a sun shelter!"
-if (chance_of_rain<=0.25 && chance_of_rain<0.75)
-  puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
+puts "Hey, it's 2019!"
